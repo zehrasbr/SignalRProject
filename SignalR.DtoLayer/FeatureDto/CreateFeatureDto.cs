5d66164434c21681dfd6efe7af2ce0fc31dad068
zehrasbr <zehraisbr@gmail.com>
2024-02-20 16:13:12 +0300

    dtos finished

--- SignalR.DtoLayer/FeatureDto/CreateFeatureDto.cs
+++ SignalR.DtoLayer/FeatureDto/CreateFeatureDto.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace SignalR.DtoLayer.FeatureDto
 {
-    internal class CreateFeatureDto
+    public class CreateFeatureDto
     {
+        public string Title1 { get; set; }
+        public string Description1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description2 { get; set; }
+        public string Title3 { get; set; }
+        public string Description3 { get; set; }
     }
 }
--- SignalR.DtoLayer/FeatureDto/UpdateFeatureDto.cs
+++ SignalR.DtoLayer/FeatureDto/UpdateFeatureDto.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SignalR.DtoLayer.FeatureDto
 {
-    internal class UpdateFeatureDto
+    public class UpdateFeatureDto
     {
+        public int FeatureID { get; set; }
+        public string Title1 { get; set; }
+        public string Description1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description2 { get; set; }
+        public string Title3 { get; set; }
+        public string Description3 { get; set; }
     }
 }
