d5fe8011b425bfdd779a8762ff5f4f19a0b35374
zehrasbr <zehraisbr@gmail.com>
2024-02-20 20:06:55 +0300

    automapper, mapping

---
+++ SignalRApi/Mapping/TestimonialMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.SocialMediaDto;
+using SignalR.DtoLayer.TestimonialDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class TestimonialMapping:Profile
+    {
+        public TestimonialMapping()
+        {
+            CreateMap<Testimonial, CreateTestimonialDto>().ReverseMap();
+            CreateMap<Testimonial, GetTestimonialDto>().ReverseMap();
+            CreateMap<Testimonial, ResultTestimonialDto>().ReverseMap();
+            CreateMap<Testimonial, UpdateTestimonialDto>().ReverseMap();
+        }
+    }
+}
