4a9f67dfe0a5f47884f17090ff45f7cd07dc9c72
zehrasbr <zehraisbr@gmail.com>
2024-02-24 13:00:33 +0300

    .

--- SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -4,7 +4,6 @@ namespace SignalRWebUI.ViewComponents.LayoutComponents
 {
 	public class _LayoutSidebarComponentPartial : ViewComponent
 	{
-
 		public IViewComponentResult Invoke()
 		{
 			return View();
