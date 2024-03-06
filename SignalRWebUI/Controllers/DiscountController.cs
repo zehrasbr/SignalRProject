4b682e233bbc8ff69b1ab2351768990e5da8c895
zehrasbr <zehraisbr@gmail.com>
2024-03-06 14:09:53 +0300

    discount

--- SignalRWebUI/Controllers/DiscountController.cs
+++ SignalRWebUI/Controllers/DiscountController.cs
@@ -1,48 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DiscountDto;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
 	public class DiscountController : Controller
 	{
 		private readonly IHttpClientFactory _httpClientFactory;
-		public AboutController(IHttpClientFactory httpClientFactory)
+		public DiscountController(IHttpClientFactory httpClientFactory)
 		{
 			_httpClientFactory = httpClientFactory;
 		}
 		public async Task<IActionResult> Index()
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7169/api/About/");
+			var responseMessage = await client.GetAsync("https://localhost:7169/api/Dicount/");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+				var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
 				return View(values);
 			}
 			return View();
 		}
 		[HttpGet]
-		public IActionResult CreateAbout()
+		public IActionResult CreateDiscount()
 		{
 			return View();
 		}
 		[HttpPost]
-		public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+		public async Task<IActionResult> CreateDiscount(CreateDiscountDto createDiscountDto)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(createAboutDto);
+			var jsonData = JsonConvert.SerializeObject(createDiscountDto);
 			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PostAsync("https://localhost:7169/api/About", stringContent);
+			var responseMessage = await client.PostAsync("https://localhost:7169/api/Discount", stringContent);
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
 			}
 			return View();
 		}
-		public async Task<IActionResult> DeleteAbout(int id)
+		public async Task<IActionResult> DeleteDiscount(int id)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.DeleteAsync($"https://localhost:7169/api/About/{id}");
+			var responseMessage = await client.DeleteAsync($"https://localhost:7169/api/Discount/{id}");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
@@ -50,25 +53,25 @@ namespace SignalRWebUI.Controllers
 			return View();
 		}
 		[HttpGet]
-		public async Task<IActionResult> UpdateAbout(int id)
+		public async Task<IActionResult> UpdateDiscount(int id)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync($"https://localhost:7169/api/About/{id}");
+			var responseMessage = await client.GetAsync($"https://localhost:7169/api/Discount/{id}");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<UpdateAboutDtos>(jsonData);
+				var values = JsonConvert.DeserializeObject<UpdateDiscountDto>(jsonData);
 				return View(values);
 			}
 			return View();
 		}
 		[HttpPost]
-		public async Task<IActionResult> UpdateAbout(UpdateAboutDtos updateAboutDtos)
+		public async Task<IActionResult> UpdateDiscount(UpdateDiscountDto updateDiscountDto)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(updateAboutDtos);
+			var jsonData = JsonConvert.SerializeObject(updateDiscountDto);
 			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PutAsync("https://localhost:7169/api/About/", stringContent);
+			var responseMessage = await client.PutAsync("https://localhost:7169/api/Discount/", stringContent);
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
