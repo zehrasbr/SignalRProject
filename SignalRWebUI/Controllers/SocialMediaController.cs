2c20f612e51b1b9620f22c2959831cb96e6247ac
zehrasbr <zehraisbr@gmail.com>
2024-03-07 13:09:41 +0300

    social media

--- SignalRWebUI/Controllers/SocialMediaController.cs
+++ SignalRWebUI/Controllers/SocialMediaController.cs
@@ -1,51 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using SignalRWebUI.Dtos.DiscountDtos;
+using SignalRWebUI.Dtos.SocialMediaDtos;
 using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
-	public class DiscountController : Controller
+	public class SocialMediaController : Controller
 	{
 		private readonly IHttpClientFactory _httpClientFactory;
-		public DiscountController(IHttpClientFactory httpClientFactory)
+		public SocialMediaController(IHttpClientFactory httpClientFactory)
 		{
 			_httpClientFactory = httpClientFactory;
 		}
 		public async Task<IActionResult> Index()
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7169/api/Dicount/");
+			var responseMessage = await client.GetAsync("https://localhost:7169/api/SocialMedia/");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+				var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
 				return View(values);
 			}
 			return View();
 		}
 		[HttpGet]
-		public IActionResult CreateDiscount()
+		public IActionResult CreateSocialMedia()
 		{
 			return View();
 		}
 		[HttpPost]
-		public async Task<IActionResult> CreateDiscount(CreateDiscountDto createDiscountDto)
+		public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(createDiscountDto);
+			var jsonData = JsonConvert.SerializeObject(createSocialMediaDto);
 			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PostAsync("https://localhost:7169/api/Discount", stringContent);
+			var responseMessage = await client.PostAsync("https://localhost:7169/api/SocialMedia", stringContent);
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
 			}
 			return View();
 		}
-		public async Task<IActionResult> DeleteDiscount(int id)
+		public async Task<IActionResult> DeleteSocialMedia(int id)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.DeleteAsync($"https://localhost:7169/api/Discount/{id}");
+			var responseMessage = await client.DeleteAsync($"https://localhost:7169/api/SocialMedia/{id}");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
@@ -53,25 +53,25 @@ namespace SignalRWebUI.Controllers
 			return View();
 		}
 		[HttpGet]
-		public async Task<IActionResult> UpdateDiscount(int id)
+		public async Task<IActionResult> UpdateSocialMedia(int id)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync($"https://localhost:7169/api/Discount/{id}");
+			var responseMessage = await client.GetAsync($"https://localhost:7169/api/SocialMedia/{id}");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<UpdateDiscountDto>(jsonData);
+				var values = JsonConvert.DeserializeObject<UpdateSocialMediaDto>(jsonData);
 				return View(values);
 			}
 			return View();
 		}
 		[HttpPost]
-		public async Task<IActionResult> UpdateDiscount(UpdateDiscountDto updateDiscountDto)
+		public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(updateDiscountDto);
+			var jsonData = JsonConvert.SerializeObject(updateSocialMediaDto);
 			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PutAsync("https://localhost:7169/api/Discount/", stringContent);
+			var responseMessage = await client.PutAsync("https://localhost:7169/api/SocialMedia/", stringContent);
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
