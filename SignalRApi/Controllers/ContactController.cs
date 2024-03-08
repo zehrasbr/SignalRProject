53931bb7644aa990d779b53ba3ae9d2d0065d184
zehrasbr <zehraisbr@gmail.com>
2024-03-08 19:43:30 +0300

    contact

--- SignalRApi/Controllers/ContactController.cs
+++ SignalRApi/Controllers/ContactController.cs
@@ -37,14 +37,14 @@ namespace SignalRApi.Controllers
             });
             return Ok("İletişim bilgisi eklendi.");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteContact(int id)
         {
             var value = _contactService.TGetByID(id);
             _contactService.TDelete(value);
             return Ok("İletişim bilgisi silindi.");
         }
-        [HttpGet("GetContact")]
+        [HttpGet("{id}")]
         public IActionResult GetContact(int id)
         {
             var value = _contactService.TGetByID(id);
