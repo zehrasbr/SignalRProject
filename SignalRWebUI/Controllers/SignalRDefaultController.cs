458dc8b272d389fc4de8061a820a92a7b8121431
zehrasbr <zehraisbr@gmail.com>
2024-03-15 21:17:42 +0300

    client connected

---
+++ SignalRWebUI/Controllers/SignalRDefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+	public class SignalRDefaultController : Controller
+	{
+		public IActionResult Indeyx()
+		{
+			return View();
+		}
+	}
+}
